9921f62787bcb2327f08b28a74e72a6c1faa2cb9
developer <developer@kali.pfe>
2023-02-05 15:25:58 -0500

    added dns

--- main.py
+++ main.py
@@ -44,11 +44,35 @@
 
 # smtp
 # https://docs.python.org/3/library/smtplib.html
-import smtplib
+#import smtplib
+#
+#server = smtplib.SMTP('10.0.2.4')
+#server.set_debuglevel(1)
+#server.login('msfadmin', 'msfadmin')
+#server.quit()
 
-server = smtplib.SMTP('10.0.2.4')
-server.set_debuglevel(1)
-server.login('msfadmin', 'msfadmin')
-server.quit()
 
+# dns using dnspython
+# https://github.com/rthalley/dnspython
+# ISC License
+# https://github.com/rthalley/dnspython/blob/master/LICENSE
+import dns.message
+import dns.rdataclass
+import dns.rdatatype
+import dns.query
 
+qname = dns.name.from_text("google.com")
+q = dns.message.make_query(qname, dns.rdatatype.NS)
+print("The query is:")
+print(q)
+print("")
+r = dns.query.udp(q, "10.0.2.4")
+print("The response is:")
+print(r)
+print("")
+print("The nameservers are:")
+ns_rrset = r.find_rrset(r.answer, qname, dns.rdataclass.IN, dns.rdatatype.NS)
+for rr in ns_rrset:
+    print(rr.target)
+print("")
+print("")
