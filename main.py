ebf72abf856b8311979c44155a77b8bf9b498e49
developer <developer@kali.pfe>
2023-02-05 15:57:47 -0500

    added rpc

--- main.py
+++ main.py
@@ -56,23 +56,34 @@
 # https://github.com/rthalley/dnspython
 # ISC License
 # https://github.com/rthalley/dnspython/blob/master/LICENSE
-import dns.message
-import dns.rdataclass
-import dns.rdatatype
-import dns.query
+#import dns.message
+#import dns.rdataclass
+#import dns.rdatatype
+#import dns.query
+#
+#qname = dns.name.from_text("google.com")
+#q = dns.message.make_query(qname, dns.rdatatype.NS)
+#print("The query is:")
+#print(q)
+#print("")
+#r = dns.query.udp(q, "10.0.2.4")
+#print("The response is:")
+#print(r)
+#print("")
+#print("The nameservers are:")
+#ns_rrset = r.find_rrset(r.answer, qname, dns.rdataclass.IN, dns.rdatatype.NS)
+#for rr in ns_rrset:
+#    print(rr.target)
+#print("")
+#print("")
 
-qname = dns.name.from_text("google.com")
-q = dns.message.make_query(qname, dns.rdatatype.NS)
-print("The query is:")
-print(q)
-print("")
-r = dns.query.udp(q, "10.0.2.4")
-print("The response is:")
-print(r)
-print("")
-print("The nameservers are:")
-ns_rrset = r.find_rrset(r.answer, qname, dns.rdataclass.IN, dns.rdatatype.NS)
-for rr in ns_rrset:
-    print(rr.target)
-print("")
-print("")
+
+# rpc port 111 (SUN Remote Procedure Call)
+# https://docs.python.org/3/library/xmlrpc.client.html
+# simple test program (from the XML-RPC specification)
+#from xmlrpc.client import ServerProxy, Error
+#
+#with ServerProxy("http://10.0.2.4:111/") as proxy:
+#    print(proxy)
+
+
