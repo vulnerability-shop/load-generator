ea9c7ee0f675ca3d211ad0802f0ab25afa3ab537
Yohan Kim <yohan.kim.1@etsmtl.net>
2023-03-30 01:26:31 -0400

    simplified try except in main

--- main.py
+++ main.py
@@ -3,35 +3,37 @@ from time import sleep
 from worker import Worker
 import signals
 
+def getFile(args) -> list:
+    if len(args) < 2:
+        raise ValueError()
+    filename = args[1]
+    if filename == None:
+        raise ValueError()
+    moduleList = []
+    file = open(filename, 'r').read().splitlines()
+    for line in file:
+        moduleList.append(line)
+    return moduleList
+
 def main(moduleList: list):
-    try:
-        workers = []
-        print('creating workers')
-        for w in moduleList:
-            workers.append(Worker(w))
-        print('starting workers')
-        for w in workers:
-            w.start()
-        while True: sleep(60)
-    except(KeyboardInterrupt, SystemExit):
-        print("sending stop signal")
-        signals.stop_signal = 1
+    workers = []
+    print('creating workers')
+    for w in moduleList:
+        workers.append(Worker(w))
+    print('starting workers')
+    for w in workers:
+        w.start()
+    while True: sleep(60)
 
 if __name__ == '__main__':
     try:
-        if len(argv) < 2:
-            raise ValueError()
-        filename = argv[1]
-        if filename == None:
-            raise ValueError()
-        moduleList = []
-        file = open(filename, 'r').read().splitlines()
-        for line in file:
-            moduleList.append(line)
-        main(moduleList)
+        main(getFile(argv))
     except ValueError:
         print("invalid arguments")
     except FileNotFoundError:
         print("file not found")
+    except(KeyboardInterrupt, SystemExit):
+        print("sending stop signal")
+        signals.stop_signal = 1
     except:
         print("unexpected error")
