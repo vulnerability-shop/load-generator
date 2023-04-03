fd641a7f49e6b42d5d6c0807b4130deb9bdd9b55
Marco Caballero <caballerom@hotmail.ca>
2023-04-03 16:22:06 -0400

    tweaks to normal login and add to cart

--- scenarios/normal_add_cart.py
+++ scenarios/normal_add_cart.py
@@ -1,43 +1,61 @@
-
 import json
 import requests
 import subprocess
 import argparse
+import random
 
 def addToCart(auth_token):
-    # double check endpoint
-    url = "http://127.0.0.1:5182/addToCart?"
+    url = "http://127.0.0.1:5182/addCartItem"
 
-    data = {
-        "username": username,
-        "password": password
+    nbItems = random.randint(1,3)
+
+    for _ in range(nbItems):
+        itemId = random.randint(1,1000)
+        itemQty = random.randint(1,10)
+
+        data = {
+            "item_id": itemId,
+            "item_quantity": itemQty
         }
+        # TODO test auth= or this: headers = {'Authorization': auth_token},
+        response = requests.post(url, json=data, auth=auth_token)
+        http_status_code = response.status_code
 
-    response = requests.post(url, json=data)
-    http_status_code = response.status_code
-
-    if http_status_code == 200:
-        auth_token = json.loads(response.text)['Token']
-        print(auth_token)
-    elif http_status_code == 401:
-        print(-1)
+        if http_status_code == 200:
+            print(f'Added {itemQty} of Item: {itemId} to cart for a random user:')
+        elif http_status_code == 401:
+            print('Login error in normal add cart')
+            exit(1)
+        else:
+            print('Error in normal add cart')
+            exit(1)
+    print(f'{nbItems} items added to cart successfully')
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("--userId", type=int, required=False, help="UserId, if null, will login as random")
     parser.add_argument("--filePath", type=str, required=False, help="user_migration csv path, if null uses default")
-
+    parser.add_argument("--userAuth", type=str, required=False, help="user token, if null will login")
     args = parser.parse_args()
 
-    if args.filePath is not None:
-        # Run fetch user for a random username and password with a specified file path
-        output = subprocess.check_output(['python', './normal_login.py', '--filePath', str(args.filePath)])
-        auth_token = output.decode('utf-8')
+    if args.userAuth is not None:
+        addToCart(args.userAuth)
     else:
-        # Run fetch user for a random username and password
-        output = subprocess.check_output(['python', './normal_login.py'])
+        if args.userId is not None and args.filePath is not None:
+            # Run normal login for a defined user id with a specified file path
+            output = subprocess.check_output(['python', './normal_login.py', '--userId', str(args.userId),
+                                              '--filePath', str(args.filePath)])
+        elif args.userId is not None:
+            # Run normal login for a defined user id
+            output = subprocess.check_output(['python', './normal_login.py', '--userId', str(args.userId)])
+        elif args.filePath is not None:
+            # Run normal login for a random user id with a specified file path
+            output = subprocess.check_output(['python', './normal_login.py', '--filePath', str(args.filePath)])
+        else:
+            # Run normal login random user id
+            output = subprocess.check_output(['python', './normal_login.py'])
         auth_token = output.decode('utf-8')
-
-    addToCart(auth_token)
+        addToCart(auth_token)
 
 
--- scenarios/normal_login.py
+++ scenarios/normal_login.py
@@ -1,4 +1,3 @@
-
 import json
 import requests
 import subprocess
@@ -11,7 +10,7 @@ def login(username, password):
     data = {
         "username": username,
         "password": password
-        }
+    }
 
     response = requests.post(url, json=data)
     http_status_code = response.status_code
@@ -29,7 +28,6 @@ if __name__ == "__main__":
     parser.add_argument("--filePath", type=str, required=False, help="user_migration csv path, if null uses default")
     parser.add_argument("--username", type=str, required=False, help="Manual username input")
     parser.add_argument("--password", type=str, required=False, help="Manual password input")
-
     args = parser.parse_args()
 
     if args.username is not None and args.password is not None:
@@ -37,17 +35,17 @@ if __name__ == "__main__":
         login(args.username, args.password)
         exit(0)
     elif args.userId is not None and args.filePath is not None:
-        # Run fetch user for a defined username and password with a specified file path
+        # Run fetch user for a defined user id with a specified file path
         output = subprocess.check_output(['python', '../modules/fetch_user.py', '--userId', str(args.userId),
                                           '--filePath', str(args.filePath)])
     elif args.userId is not None:
-        # Run fetch user for a defined username and password
+        # Run fetch user for a defined user id
         output = subprocess.check_output(['python', '../modules/fetch_user.py', '--userId', str(args.userId)])
     elif args.filePath is not None:
-        # Run fetch user for a random username and password with a specified file path
+        # Run fetch user for a random user id with a specified file path
         output = subprocess.check_output(['python', '../modules/fetch_user.py', '--filePath', str(args.filePath)])
     else:
-        # Run fetch user for a random username and password
+        # Run fetch user for a random user id
         output = subprocess.check_output(['python', '../modules/fetch_user.py'])
 
     output_list = output.decode('utf-8').split()
